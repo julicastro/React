ee76910640064debfd755d8f9e0a768e894c07d6
julicastro <castroejulian@gmail.com>
2021-09-27 23:59:37 -0300

    Pixabay API Request

--- image-list/src/components/App.js
+++ image-list/src/components/App.js
@@ -1,21 +1,30 @@
 import React from 'react';
+import axios from 'axios';
 import SearchInput from './Searchinput';
+import ImageList from './ImageList';
+
+
+
 
 class App extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props)
+        this.state = { images: [] }
         this.onSearchSubmit = this.onSearchSubmit.bind(this)
     }
 
-    onSearchSubmit(entry){
-        console.log(entry)
+
+    async onSearchSubmit(entry) {
+        const response = await axios.get(`https://pixabay.com/api/?key=23593155-6110516b279c1b3c5aacc66a9&q=${entry}&image_type=photo`)
+        this.setState({ images: response.data.hits })
     }
 
     render() {
         return (
             <div className='ui container' style={{ marginTop: '30px' }}>
-                <SearchInput onSearchSubmit={this.onSearchSubmit}/>
+                <SearchInput onSearchSubmit={this.onSearchSubmit} />
+                <ImageList images={this.state.images} />
             </div>
         )
     }
