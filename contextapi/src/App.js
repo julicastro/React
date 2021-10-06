11aad91a74c05827bdc82aeae60995c2b9866250
julicastro <castroejulian@gmail.com>
2021-10-06 01:01:42 -0300

    Multiple contexts

--- contextapi/src/App.js
+++ contextapi/src/App.js
@@ -2,15 +2,21 @@ import React from "react";
 import Navbar from "./Components/Navbar";
 import TodoList from "./Components/TodoList";
 import ThemeContextProvider from "./Context/ThemeContext";
+import AuthContextProvider from "./Context/AuthContext";
+
 
 function App() {
   return (
     <div className="App">
       <div className='ui raised very padded text container segment'>
-        <ThemeContextProvider>
-          <Navbar />
-          <TodoList />
-        </ThemeContextProvider>
+        <AuthContextProvider>
+
+          <ThemeContextProvider>
+            <Navbar />
+            <TodoList />
+          </ThemeContextProvider>
+        </AuthContextProvider>
+
       </div>
     </div>
   );
