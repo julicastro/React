69df6de1f4b703a92169e3dac3e87d8dc7ba2a17
julicastro <castroejulian@gmail.com>
2021-10-07 03:43:57 -0300

    hooks

---
+++ 07-hooks/src/Components/TodoList.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import AddNewTodo from "./AddNewTodo";
+
+
+const TodoList = () => {
+
+    const [todos, setTodos] = useState([
+        { text: 'Pay bills', id: 1 },
+        { text: 'Do your homework', id: 2 },
+        { text: 'Feed the dog', id: 3 }
+    ]);
+
+    const addTodo = (text) => {
+        setTodos([
+            ...todos,
+            { text: text, id: Math.random() }
+        ]);
+    };
+
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {    
+
+    }, [todos]); // useEffect solo funciona para todos. no para count
+
+    useEffect(() => {    
+
+    }, [count]); // este solo funciona para count
+
+
+    return (
+        <div>
+            <ul>
+                {todos.map((todo) => {
+                    return (
+                        <li key={todo.id}>{todo.text}</li>
+                    )
+                })}
+            </ul>
+            <AddNewTodo addTodo={addTodo}/>
+            <button onClick={() => setCount(count + 1)}>Score: {count} </button>
+        </div>
+    )
+};
+
+export default TodoList;
---
+++ 08-context-hooks/src/Components/TodoList.js
@@ -0,0 +1,19 @@
+import React, { useContext} from "react";
+import { ThemeContext } from "../Context/ThemeContext";
+
+const TodoList = () => {
+
+    const { isDarkTheme, darkTheme, lightTheme, changeTheme } = useContext(ThemeContext);
+    const theme = isDarkTheme ? darkTheme : lightTheme;
+    
+    return (
+        <div style={{ background: theme.background, color: theme.text, height: '140px', textAlign: 'center' }} className='ui list'>
+            <p className='item'>Plan the family trip</p>
+            <p className='item'>Go for shopping for dinner</p>
+            <p className='item'>Go for a walk</p>
+            <button className="ui button primary" onClick={changeTheme}>Change theme</button>
+        </div>
+    );
+};
+
+export default TodoList;
